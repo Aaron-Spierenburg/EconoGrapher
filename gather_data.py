298d05a0f9d5f4808a8052d395ac79fb3195efad
Andrew Booth <abooth15@gmail.com>
2024-04-09 16:10:34 -0400

    Updated gather_data.py to only update files, not fetch data for entire new dataset
    -added comments and more infomation for usage
    -removed gather_data.ipynb

--- gather_data.py
+++ gather_data.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 
-def toDecimalConversion(number,quant=20):
+def toDecimalConversion(number,quant=6):
     'Convert to Decimal for accuracy lost in float-types'
     quant = decimal.Decimal(str(quant))
     quant = 1*10**(-quant)
@@ -18,43 +18,68 @@ def toDecimalConversion(number,quant=20):
     return decimal_return
 
 def event_sources():
-        with open(r'events_sources.json', 'r') as json_file:
-            sources_json=json.load(json_file)
-        links=[]
-        for k,v in sources_json.items():
-                for k2,v2 in v.items():
-                    links.append((k,k2,v2))
-        return links
+    '''Retrieve a list of event details from json file
+    \nreturns a tuple:
+    \n(country,event_name,link)'''
+    with open(r'events_sources.json', 'r') as json_file:
+        sources_json=json.load(json_file)
+    links=[]
+    for k,v in sources_json.items():
+            for k2,v2 in v.items():
+                links.append((k,k2,v2))
+    return links
 
-def check_end_of_consensus(source:str)->bool:
-    'check if last entry has consensus after clicking "see more"'
+
+
+def data_check(source:str,date_check=None)->bool:
+    'check if last entry has consensus after clicking "see more" or if the data is newer than our last update'
+    if date_check:
+        date_check = pd.to_datetime(date_check)
     soup = bs4.BeautifulSoup(source,'html.parser')
     source = soup.prettify()
     info = re.findall('<tr.*?historicEvent.*?</tr>',source,re.DOTALL)
     tries=0
     for i in info[::-1]:#reverse order
-        consensus_re = re.search('.*?</td>.*?</td>.*?</td>.*?<td.*?\n(.*?)\n.*?</td>',i,re.DOTALL).group(1)
-        consensus_re = re.sub('[^0-9.]','',consensus_re)
-        try:
-            toDecimalConversion(consensus_re)
-            return False
-        except decimal.InvalidOperation :
-            tries+=1
-        if tries<16:
-            continue
+        if date_check:
+            month_re = re.search('<td.*?\n(.*?)\n.*?</td>',i,re.DOTALL).group(1)
+            month_re = month_re.strip()
+            try:
+                month_re = month_re[:month_re.index('(')-1]
+            except:
+                pass
+            month_re = pd.to_datetime(month_re)
+            if month_re<date_check:
+                return True
+            else:
+                return False
         else:
-            return True
+            consensus_re = re.search('.*?</td>.*?</td>.*?</td>.*?<td.*?\n(.*?)\n.*?</td>',i,re.DOTALL).group(1)
+            consensus_re = re.sub('[^0-9.]','',consensus_re)
+            try:
+                toDecimalConversion(consensus_re)
+                return False
+            except decimal.InvalidOperation :
+                tries+=1
+            if tries<16:
+                continue
+            else:
+                return True
+
+
 
 
 def extract_from_html(source:str,country,event_name):
     '''Function that takes the source html and converts it to a csv for analysis on the graphing file
-    \nIs called when the parser determines there are no longer any releases with a consensus value
+    \nIs called when the parser determines there are no longer any releases with a consensus value, or no more new data
     \nsource: is passed the driver.page_source information with is html string
     \ncountry & event_name: are determined by the event_sources.json file to assist in adding files to Data/
     '''
-    extractDF = pd.DataFrame(columns=['Date','Time','Actual','Consensus'])
+
+    extractDF = pd.DataFrame(columns=['Date','Time','Actual','Consensus'])#create empty dataframe
     soup = bs4.BeautifulSoup(source,'html.parser')
-    source = soup.prettify()
+    source = soup.prettify()#facilitate parsing with formatted html
+
+    #parse html and retrieve relevant data adding to the end of the dataframe one row at a time
     info = re.findall('<tr.*?historicEvent.*?</tr>',source,re.DOTALL)
     for i in info:
         month_re = re.search('<td.*?\n(.*?)\n.*?</td>',i,re.DOTALL)
@@ -63,7 +88,6 @@ def extract_from_html(source:str,country,event_name):
         consensus_re = re.search('.*?</td>.*?</td>.*?</td>.*?<td.*?\n(.*?)\n.*?</td>',i,re.DOTALL).group(1)
         actual_re = re.sub('[^0-9.]','',actual_re)
         consensus_re  = re.sub('[^0-9.]','',consensus_re)
-        
         try:
             actual_re = toDecimalConversion(actual_re,6)
             consensus_re = toDecimalConversion(consensus_re,6)
@@ -73,15 +97,13 @@ def extract_from_html(source:str,country,event_name):
         row = {'Date':month_re.group(1),'Time':time_re,'Actual':actual_re,'Consensus':consensus_re}
         extractDF.loc[len(extractDF)] = row
         
+    #create a difference
     extractDF['Difference'] = extractDF['Actual'] - extractDF['Consensus']
-    
     extractDF['Actual'] = extractDF['Actual'].apply(pd.to_numeric)
-
     extractDF['Consensus'] = pd.to_numeric(extractDF['Consensus'],errors='coerce')
 
-    
-
-    dst_str = {
+    #handling daylight savings time and converting the times to gmt+2 for MetaTrader time/ forex time
+    dst_str = { #dict of time changes for each year to our oldest point of having consensus data, 2008
         2024:('2024-03-10','2024-11-03'),
         2023:('2023-03-12','2023-11-05'),
         2022:('2022-03-13','2022-11-06'),
@@ -100,6 +122,7 @@ def extract_from_html(source:str,country,event_name):
         2009:('2009-03-08','2009-11-01'),
         2008:('2008-03-09','2008-11-02'),
         }
+    #converting the values into timestamps 
     dst = {}
     for i in dst_str:
         temptup = []
@@ -108,21 +131,26 @@ def extract_from_html(source:str,country,event_name):
             temptup.append(j)
         dst[i] = temptup
     extractDFDates = extractDF.copy()
+
+    #handling data rows with different formatting and adding the times 
     for i,r in extractDF.iterrows():
         if '(' in r["Date"]:
             date = re.search('(.*?)\(.*',r['Date'])
             newdate = date.group(1).strip()
         else:
             newdate = r['Date']
-        
         newdate = f'{newdate} {r["Time"]}'
         newdate = pd.to_datetime(newdate)
+
+        #adding the hours to the base date depending on the daylight savings time status
         if newdate>dst[newdate.year][0] and newdate<dst[newdate.year][1]:
             newdate = newdate + pd.to_timedelta('8 hours')
         else:
             newdate = newdate + pd.to_timedelta('7 hours')
         extractDFDates['Date'].at[i] = newdate
     extractDFDates.drop(columns=['Time'],inplace=True) 
+
+
     try:
         os.makedirs('Data')
     except:
@@ -130,34 +158,54 @@ def extract_from_html(source:str,country,event_name):
     try:
         os.makedirs(f'Data\\{country}')
     except:
-        pass
+        if os.path.isfile(f'Data\\{country}\\{event_name}.csv'):
+            old_df = pd.read_csv(f'Data\\{country}\\{event_name}.csv',index_col=0,parse_dates=['Date'])
+            extractDFDates= pd.concat([extractDFDates,old_df]).drop_duplicates(subset=['Date'],ignore_index=True)
+            extractDFDates['Difference'] = extractDFDates['Difference'].apply(toDecimalConversion)
     extractDFDates.to_csv(f'Data\\{country}\\{event_name}.csv')
+
+
+
+
 
 
 def gather_data(event:int=0,clicks:int=0):
     'function to access website, and retrieve the html source code, passing on to extract_from_html()'
+    
     def x_button_find():
+        'function to close pop-up when scraping'
         x_button = WebDriverWait(driver, 0.4).until(
             EC.element_to_be_clickable((By.XPATH, '//i[@class="popupCloseIcon largeBannerCloser"]')))
         if x_button:
             x_button.click()
         return 1
     
+    #access event information and sources from json file
     links = event_sources()
     events_to_get = len(links)
-    link:str = links[event][2]
-    link_num = link[link.index('-',-4)+1:]
+    link:str = links[event][2]#third tuple item is the link
+    link_num = link[link.index('-',-4)+1:]#extract ID number of page to locate the see more button as it is ID specific 
+
+    #check to see if there is already data, and if there is retrieve the latest date of that file
+    newest_event_date =None
+    if os.path.isfile(f'Data\\{links[event][0]}\\{links[event][1]}.csv'):
+        with open(f'Data\\{links[event][0]}\\{links[event][1]}.csv','r') as data_file:
+            for idx,line in enumerate(data_file):
+                if idx == 1:
+                    items = line.split(',')
+                    newest_event_date = items[1]
+                    break
+    #open webpage and load for 6 seconds before beginning scraping
     driver = webdriver.Chrome()
     driver.set_page_load_timeout(6)
-
-
     try:
         driver.get(link)
     except TimeoutException:
-        print('bypass load')
+        pass
+    #beginning of scraping the current link
     try:
         while True:
-            if clicks==0:
+            if clicks==0:#check if clicked the pop-up 
                 try:
                     clicks = x_button_find()
                 except:
@@ -167,14 +215,14 @@ def gather_data(event:int=0,clicks:int=0):
             if see_more_button:
                 try:
                     see_more_button.click()
-                except ElementClickInterceptedException:
+                except ElementClickInterceptedException:#if popup blocks see more button
                     clicks = x_button_find()
                     continue
-                if check_end_of_consensus(driver.page_source):
+                if data_check(driver.page_source,newest_event_date if newest_event_date else None): #preform check on end of data if the last 15 table rows have consensus data
                     html = driver.page_source
                     driver.quit()
-                    extract_from_html(html,links[event][0],links[event][1])
-                    if event <events_to_get-1:
+                    extract_from_html(html,links[event][0],links[event][1])#format html file into a dataframe and then create csv
+                    if event <events_to_get-1:#if there are still links to check run the function again 
                         gather_data(event=(event+1))
                         break
                     else: break
